6de3bbcb2ae334e0e92138d9d6d8c45a37d31a44
danielslapsenietis <daniels.lapsenietis@gmail.com>
2025-03-20 07:02:08 +0200

    Update project_euler_7.py

--- project_euler_7.py
+++ project_euler_7.py
@@ -1,18 +1,21 @@
-def prime_number_in_a_row(n):
-    prime_number_list = []
-    x = list(range(2, 1000000))
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-    for i in x:
-        is_prime = True
-        for j in range(2, int(i**0.5) + 1):  # Only check up to sqrt(i) for efficiency
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            prime_number_list.append(i)
-            if len(prime_number_list) == n:
-                return prime_number_list[-1]
+def find_nth_prime(nth):
+    """Find the nth prime number."""
+    count, num = 0, 1
+    
+    while count < nth:
+        num += 1
+        if is_prime(num):
+            count += 1
+    
+    return num
 
-print(prime_number_in_a_row(10001))
-
-############### SOLVED WITH HELP OF CHATGPT ###############
+print(find_nth_prime(10001))
